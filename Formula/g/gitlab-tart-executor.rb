8ac1654bfc5a5959e07afbc53a4f8819274252d0
OTSGE <199041525+otsge@users.noreply.github.com>
2026-07-10 15:52:56 +0000

    gitlab-tart-executor (new formula)

---
+++ Formula/g/gitlab-tart-executor.rb
@@ -0,0 +1,36 @@
+# typed: false
+# frozen_string_literal: true
+
+class GitlabTartExecutor < Formula
+  desc "GitLab Runner Executor to run jobs inside isolated Tart VMs"
+  homepage "https://github.com/cirruslabs/gitlab-tart-executor"
+  version "1.28.0"
+  license "MIT"
+
+  livecheck do
+    url :homepage
+    strategy :github_latest
+  end
+
+  depends_on :macos
+  depends_on "otsge/draft/tart"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/cirruslabs/gitlab-tart-executor/releases/download/#{version}/gitlab-tart-executor-darwin-arm64.tar.gz"
+    sha256 "5b7dea840cdbabcbf3806da1d95d58c3b125dc6385b9eace86fb746c52796103"
+  end
+
+  def install
+    bin.install "gitlab-tart-executor"
+  end
+
+  def caveats
+    <<~EOS
+      See the Github repository for more information
+    EOS
+  end
+
+  test do
+    system bin/"gitlab-tart-executor", "--version"
+  end
+end
