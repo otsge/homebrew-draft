bd273e057de10f90571458cdf57bd626466b507c
OTSGE <199041525+otsge@users.noreply.github.com>
2026-07-17 11:02:23 +0000

    libtorrent-rasterbar@2.0 (new formula)

---
+++ Formula/lib/libtorrent-rasterbar@2.0.rb
@@ -0,0 +1,68 @@
+class LibtorrentRasterbarAT20 < Formula
+  desc "C++ bittorrent library with Python bindings"
+  homepage "https://www.libtorrent.org/"
+  url "https://github.com/arvidn/libtorrent/releases/download/v2.0.13/libtorrent-rasterbar-2.0.13.tar.gz"
+  sha256 "892cb75c06318e2420de0faf9f63a908069d3d237676e2459fd30abe0cb3b1bf"
+  license "BSD-3-Clause"
+  head "https://github.com/arvidn/libtorrent.git", branch: "RC_2_0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(2\.0(?:\.\d+)+)$/i)
+    strategy :github_releases
+  end
+
+  depends_on "cmake" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "boost"
+  depends_on "boost-python3"
+  depends_on "openssl@3"
+  depends_on "python@3.14"
+
+  def install
+    # Work around Homebrew's prefix scheme, which makes Python's reported
+    # site-packages path absolute and outside the keg.
+    site_packages = prefix/Language::Python.site_packages("python3.14")
+    inreplace "bindings/python/CMakeLists.txt", "${_PYTHON3_SITE_ARCH}", site_packages
+
+    args = %W[
+      -DCMAKE_CXX_STANDARD=14
+      -Dencryption=ON
+      -Dpython-bindings=ON
+      -Dpython-egg-info=ON
+      -DCMAKE_INSTALL_RPATH=#{lib}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    libexec.install "examples"
+  end
+
+  test do
+    args = [
+      "-I#{Formula["boost"].include}/boost",
+      "-L#{Formula["boost"].lib}",
+      "-I#{include}",
+      "-L#{lib}",
+      "-lpthread",
+      "-ltorrent-rasterbar",
+    ]
+
+    if OS.mac?
+      args += [
+        "-framework",
+        "SystemConfiguration",
+        "-framework",
+        "CoreFoundation",
+      ]
+    end
+
+    system ENV.cxx, libexec/"examples/make_torrent.cpp",
+                    "-std=c++14", *args, "-o", "test"
+    system "./test", test_fixtures("test.mp3"), "-o", "test.torrent"
+    assert_path_exists testpath/"test.torrent"
+
+    system "python3.14", "-c", "import libtorrent"
+  end
+end
--- Formula/t/tart-guest-agent.rb
+++ Formula/t/tart-guest-agent.rb
@@ -4,7 +4,6 @@
 class TartGuestAgent < Formula
   desc "Guest agent for Tart VMs"
   homepage "https://github.com/openai/tart-guest-agent"
-  version "0.11.0"
   license "FSL-1.1-ALv2"
 
   livecheck do
@@ -13,17 +12,17 @@ class TartGuestAgent < Formula
   end
 
   if OS.mac?
-    url "https://github.com/openai/tart-guest-agent/releases/download/v#{version}/tart-guest-agent-darwin-all.tar.gz"
+    url "https://github.com/openai/tart-guest-agent/releases/download/v0.11.0/tart-guest-agent-darwin-all.tar.gz"
     sha256 "28e1b698c726c5f1d0f8c9d086b3390f6407d47e3f4b83081d9b2a9a2a1f0bc7"
   end
 
   if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/openai/tart-guest-agent/releases/download/v#{version}/tart-guest-agent-linux-amd64.tar.gz"
+    url "https://github.com/openai/tart-guest-agent/releases/download/v0.11.0/tart-guest-agent-linux-amd64.tar.gz"
     sha256 "0b2dab17ffa74a05f47837131cb7249cd0912ddaf5a20aa00a600b7e2d08dc01"
   end
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/openai/tart-guest-agent/releases/download/v#{version}/tart-guest-agent-linux-arm64.tar.gz"
+    url "https://github.com/openai/tart-guest-agent/releases/download/v0.11.0/tart-guest-agent-linux-arm64.tar.gz"
     sha256 "2b8d238dfb87445dfc66b8839ce72eb4c8193e034449291a69fa61f5b7753f96"
   end
 
