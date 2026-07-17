ca73107abc9cb5e3d6ba8cdcfd2aa23f5655bc30
OTSGE <199041525+otsge@users.noreply.github.com>
2026-07-17 11:42:50 +0000

    libtorrent-rasterbar@2.0: add 2.0.13 bottle.

--- Formula/lib/libtorrent-rasterbar@2.0.rb
+++ Formula/lib/libtorrent-rasterbar@2.0.rb
@@ -12,6 +12,17 @@ class LibtorrentRasterbarAT20 < Formula
     strategy :github_releases
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/otsge/draft"
+    sha256 cellar: :any, arm64_tahoe:   "347cbb7d1a4a42b23a8b8cb2e3df12f2bb42350c340044c2cc0581a0c8d65d5d"
+    sha256 cellar: :any, arm64_sequoia: "1d0b3308623e784020e4e24a8e57487808141b63c478d514e65765149da3665a"
+    sha256 cellar: :any, arm64_sonoma:  "ff761eff2ae2a9db33dd09580a4692d6b347c4853b517018971bfae5ada6eb12"
+    sha256 cellar: :any, tahoe:         "fefb1ccfcd5657b6c6bfbaf1d093bd742bade5a61b29a1a6812909e067476123"
+    sha256 cellar: :any, sequoia:       "6641dc008744b356875cd8b77640bc68ed90e6a0b8f70febd6a9e899b72d91d9"
+    sha256 cellar: :any, arm64_linux:   "858166d6cef9032e0b094c04f58856898822be94f90f09d5d8f5b84410874845"
+    sha256 cellar: :any, x86_64_linux:  "bc074cfb4de7879375ed7f81bbc6943b4878ef2b8f4c27953ef53b7e63334299"
+  end
+
   depends_on "cmake" => :build
   depends_on "python-setuptools" => :build
   depends_on "boost"
