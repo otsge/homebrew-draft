adfe66be8b6a7975b2668d6fd3cc1bbc2f6c42f9
OTSGE <199041525+otsge@users.noreply.github.com>
2026-07-17 18:11:03 +0000

    Use openssl@4 formula from homebrew/core

--- Formula/lib/libssh2.rb
+++ Formula/lib/libssh2.rb
@@ -32,7 +32,7 @@ class Libssh2 < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on "otsge/draft/openssl@4"
+  depends_on "openssl@4"
 
   on_linux do
     depends_on "zlib-ng-compat"
