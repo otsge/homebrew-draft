4ec69b5fb00b70ab7635403f3fadb6dfe29255ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-11 02:25:43 +0000

    softnet 0.22.1
    Closes #28.
    
    Signed-off-by: OTSGE <199041525+otsge@users.noreply.github.com>

--- Formula/s/softnet.rb
+++ Formula/s/softnet.rb
@@ -4,8 +4,8 @@
 class Softnet < Formula
   desc "Software networking with isolation for Tart"
   homepage "https://github.com/openai/softnet"
-  url "https://github.com/openai/softnet/releases/download/0.22.0/softnet.tar.gz"
-  sha256 "e4eb07c62f2dfeacceea0524967a53ef9f2a7572464118cfbea6811b4ab836e7"
+  url "https://github.com/openai/softnet/releases/download/0.22.1/softnet.tar.gz"
+  sha256 "1093be6f77b0c7a75b31cf651ff46844a81d73568184e807494f555280684644"
   license "FSL-1.1-ALv2"
 
   define_method(:install) do
